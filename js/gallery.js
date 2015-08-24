a14ca889ecb65ea7bc1767828ad6e440dcf2bffb
Larry Babcock <larryshawn@gmail.com>
2015-08-23 23:16:28 -0700

    fixed it

--- js/gallery.js
+++ js/gallery.js
@@ -18,7 +18,7 @@ function handleClickEvents() {
   // If any movie thumbnail gets clicked, slide the mask curtain down.
   $('.movie-thumbnail').click(function(e) {
     var mask = $(e.target).children('.mask');
-    mask.css('display', 'relative');
+    mask.css('display', 'block');
     mask.css('transform', 'translateY(0)');
   });
 
